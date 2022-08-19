52813fd904ee86996a8cc765415c67f9afe2b221
sakshitmeshram <sakshitmeshram2001@gmail.com>
2022-08-19 21:12:12 +0530

    Program to calculate tax on given salary Even Odd program

---
+++ Bootcoding/src/EvenOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num%2==0)
+        {
+            System.out.println("Number is even");
+        }
+        else
+        {
+            System.out.println("Number is Odd");
+        }
+
+    }
+}
