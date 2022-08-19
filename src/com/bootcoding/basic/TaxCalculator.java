39bf85526f5e9647e4c348bc76af771ed4dc3672
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2022-08-19 21:48:01 +0530

    - rename slabfirst variable

--- src/com/bootcoding/basic/TaxCalculator.java
+++ src/com/bootcoding/basic/TaxCalculator.java
@@ -8,7 +8,7 @@ public class TaxCalculator {
 
         double slab;
         double a;
-        double slabfirst;
+        double slabFirst;
         System.out.println("enter your ANNUAL income");
 
         Scanner sc = new Scanner(System.in);
@@ -18,14 +18,14 @@ public class TaxCalculator {
             System.out.println("income tax is " + a);
         }
         if (slab >= 500000 && slab <= 900000) {
-            slabfirst = slab - 500000;
-            a = slabfirst * 0.2;
+            slabFirst = slab - 500000;
+            a = slabFirst * 0.2;
             a = a + 50000;
             System.out.println("income tax is " + a);
         }
         if (slab >= 1000000) {
-            slabfirst = slab - 1000000;
-            a = slabfirst * 0.3;
+            slabFirst = slab - 1000000;
+            a = slabFirst * 0.3;
             a = a + 1500000;
             System.out.println("income tax is " + a);
         }
