245045379f74778b17dbdfc5c9e5a8a6a8dcb3f3
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2022-09-14 20:59:17 +0530

    Added code for java interface

---
+++ src/com/bootcoding/oops/implement/Application.java
@@ -0,0 +1,10 @@
+package com.bootcoding.oops.implement;
+
+public class Application {
+    public static void main(String[] args) {
+        Hero hr=new Hero();
+        FireFox ff=new FireFox();
+        hr.test();
+        ff.test();
+    }
+}
---
+++ src/com/bootcoding/oops/implement/FireFox.java
@@ -0,0 +1,8 @@
+package com.bootcoding.oops.implement;
+
+public class FireFox implements Cycle{
+    @Override
+    public void test() {
+        System.out.println("FirFox Cycle tested");
+    }
+}
