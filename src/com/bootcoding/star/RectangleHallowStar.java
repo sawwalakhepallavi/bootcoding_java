99e7740e4b7323e41ab407fb2433800ef9281a28
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2022-10-03 20:48:23 +0530

    star program

---
+++ src/com/bootcoding/star/RectangleHallowStar.java
@@ -0,0 +1,16 @@
+package com.bootcoding.star;
+
+public class RectangleHallowStar {
+    public static void main(String[] args) {
+
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4-i;j++){
+
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
