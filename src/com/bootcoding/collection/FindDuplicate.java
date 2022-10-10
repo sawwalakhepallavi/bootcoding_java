5251d982551a16cfbb16281759c7b07bd1986eb6
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2022-10-10 20:23:59 +0530

    Hashmap program

---
+++ src/com/bootcoding/collection/FindDuplicate.java
@@ -0,0 +1,33 @@
+package com.bootcoding.collection;
+
+import java.util.HashMap;
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        int []arr={5,10,5,10,15,5};
+        FindDuplicate fd=new FindDuplicate();
+        System.out.println(fd.dupliFind(arr));
+    }
+    public int dupliFind(int []arr) {
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (countMap.containsKey(arr[i])) {
+                int v = countMap.get(arr[i]);
+                countMap.put(arr[i], v + 1);
+            }
+            else {
+                countMap.put(arr[i], 1);
+            }
+        }
+        int count=0;
+        for(Integer value:countMap.values()){
+            if(value>1){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+}
