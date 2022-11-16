d6076403b39d366eaee1f138e1c8f21b4abcdecc
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2022-11-16 20:32:30 +0530

    SumFile Handeling program

--- src/com/bootcoding/file/SumFileExample.java
+++ src/com/bootcoding/file/SumFileExample.java
@@ -13,10 +13,16 @@ public class SumFileExample {
                 String num= scanner.nextLine();
                 String[] nums=num.split(",");
                 int sum=0;
+                int substraction=0;
+                int mult=1;
                 for(int i=0;i<nums.length;i++){
                     sum = sum + Integer.parseInt(nums[i]);
+                    mult = mult * Integer.parseInt(nums[i]);
+                    substraction = substraction - Integer.parseInt(nums[i]);
                 }
                 System.out.println("Addition is :"+sum);
+                System.out.println("Multiplication is :"+mult);
+                System.out.println("substraction is :"+substraction);
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
